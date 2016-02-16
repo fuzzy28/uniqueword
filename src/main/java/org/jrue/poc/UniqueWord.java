46c3e9ee9aa58db04f378e913948f3533a837cdb
Joel F. Ruelos; <ruelosjoel@gmail.com;>
2016-02-16 13:43:28 +0800

    Add comments

--- src/main/java/org/jrue/poc/UniqueWord.java
+++ src/main/java/org/jrue/poc/UniqueWord.java
@@ -6,24 +6,28 @@ package org.jrue.poc;
  * 
  */
 public class UniqueWord {
-	
+
 	public static String get(String str) {
 		String word = str.toLowerCase();
 		String candidate = "";
+		// divide the given word starting from half length of the word up to 2
 		main: for (int i = word.length() / 2; i > 1; i--) {
-			String currentWord = word.substring(0, i);
+			String preivousWord = word.substring(0, i);
+			// variable that will hold if words divided are equal to each other
 			boolean consistent = false;
 			for (int j = i; j < word.length(); j += i) {
+				//check if the previous word is equal to the current word
 				if ((j + i) <= word.length()
-						&& currentWord.equals(word.substring(j, j + i))) {
+						&& preivousWord.equals(word.substring(j, j + i))) {
 					consistent = true;
 				} else {
+					//if it's not equal then continue to split the word again
 					consistent = false;
 					continue main;
 				}
 			}
 			if (consistent)
-				candidate = currentWord;
+				candidate = preivousWord;
 		}
 		return candidate.length() > 0 ? candidate : word;
 	}
